2e069ed39838bfd28fb1eabf6d0f80c0f5d43351
Fatih Yıldız <0m.fatih.yildiz@gmail.com>
2022-02-06 00:00:56 +0300

    header revize

---
+++ src/components/Header/Links.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex items-center h-full relative w-full max-w-sm">
+    <a @click="active = 'Grades'"
+      :class="{ 'text-sky-500 hover:text-sky-500': active == 'Grades' }"
+      href="#"
+      class="w-1/3 flex justify-center text-sm font-semibold text-gray-400 hover:text-gray-800 transition duration-400"
+      >Grades</a
+    >
+    <a @click="active = 'Schedule'"
+      href="#"
+      :class="{ 'text-sky-500 hover:text-sky-500': active == 'Schedule' }"
+      class="w-1/3 flex justify-center text-sm font-semibold text-gray-400 hover:text-gray-800 transition duration-400"
+      >Schedule</a
+    >
+    <a @click="active = 'Comunnity'"
+      href="#"
+      :class="{ 'text-sky-500 hover:text-sky-500': active == 'Comunnity' }"
+      class="w-1/3 flex justify-center text-sm font-semibold text-gray-400 hover:text-gray-800 transition duration-400"
+      >Comunnity</a
+    >
+    <span
+      ref="bar"
+      class="w-1/3 bg-sky-500 absolute h-0.5 bottom-0 shadow shadow-sky-500/20 transform translate-y-0.5"
+    ></span>
+  </div>
+</template>
+
+<script>
+import anime from "animejs/lib/anime.es.js";
+export default {
+  data() {
+    return {
+      active: "Comunnity",
+      naValue: 0,
+    };
+  },
+  methods: {
+    activeBar() {
+      anime({
+        targets: this.$refs.bar,
+        translateX: -this.naValue + "%",
+        translateY: '0.125rem',
+        left: this.naValue + "%",
+        duration: 500,
+        easing: "easeInOutElastic(1, .8)",
+      });
+    },
+  },
+  watch: {
+    active(newValue) {
+      this.naValue = newValue === "Grades" ? 0 : newValue === "Schedule" ? 50 : 100;
+      this.activeBar();
+    },
+  },
+};
+</script>
---
+++ src/components/Header/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="flex bg-gray-50 focus-within:bg-gray-100 h-14 w-72 px-4 items-center rounded-xl flex-shrink-0"
+  >
+    <input
+      class="text-gray-700 focus:text-black bg-transparent outline-none border-none font-medium text-sm flex justify-between w-full focus:ring-0"
+      type="text"
+      placeholder="Type to search"
+    />
+    <a
+      href="#"
+      class="bg-white h-10 w-10 rounded-xl flex items-center justify-center border border-gray-200 flex-shrink-0"
+    >
+      <svg
+        class="text-gray-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
